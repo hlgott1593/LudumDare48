9f2199453182030903410e957bbbb4c12e622db4
Harrison Gottlieb <hlgott1593@gmail.com>
2021-04-23 20:15:18 -0700

    basic tileset test

---
+++ Assets/Tiled/rocktest_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="rocktest_tileset" tilewidth="16" tileheight="16" tilecount="18" columns="6">
+ <image source="../Textures/rocks.png" trans="ff00ff" width="96" height="48"/>
+</tileset>
